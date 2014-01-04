26cfdc0b8eb632de39f996e1d09e853604b3b9fe
coreen <coreeny@cs.washington.edu>
2014-01-04 11:02:22 -0800

    Added unit test cases and descriptive comments.

--- twice.pl
+++ twice.pl
@@ -1,5 +1,24 @@
 /* Coreen Yuen */
 
+
 /* twice rule returns true if second argument is a list with 0 or more occurences of the first argument */
+/* edge case */
 twice(_,[]).
+/* general case */
 twice(X,[X|Ys]) :- twice(X,Ys).
+
+
+/* unit tests */
+:- begin tests(twice).
+
+/* empty list, should succeed */
+test(twice1,[nondet]) :- twice(hello,[]).
+/* list with a non-repeat, should fail */
+test(twice2,[fail]) :- twice(world,[world,world,hello]).
+/* list with only repeats, should succeed */
+test(twice3,[nondet] :- twice(hello,[hello,hello,hello,hello]).
+
+:- end_tests(twice).
+
+/* run the unit tests */
+:- run_tests.
